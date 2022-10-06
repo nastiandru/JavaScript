b7d6c14aa1a200d5cec4e5b93742c36b9322bf92
ANASTASIIA ANDRUSHKEVYCH <anastasiia.andrushkevych@microsoft.wsei.edu.pl>
2022-10-06 12:22:27 +0200

    changes

--- project1/main.js
+++ project1/main.js
@@ -1,7 +1,10 @@
 const liczba1 = document.querySelector('#liczba')
 consile.dir(liczba1)
 const przeliczBtn = document.querySelector('#przelicz')
+const WynikiPojemnik = document.querySelector('#wyniki')
+
 przeliczBtn.addEventListener('click', () => {
     console.log(liczba1.value)
+    WynikiPojemnik.innerHTML = liczba1.value
 })
 //Math.min(), .max()
