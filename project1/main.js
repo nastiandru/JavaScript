25f5a3651260c4f9971d79609c24645ea27391e3
ANASTASIIA ANDRUSHKEVYCH <ANASTASIIA.ANDRUSHKEVYCH@microsoft.wsei.edu.pl>
2022-10-06 12:39:59 +0200

    change in main.js

--- project1/main.js
+++ project1/main.js
@@ -1,10 +1,12 @@
-const liczba1 = document.querySelector('#liczba')
-consile.dir(liczba1)
+const liczba1 = document.querySelector('#liczba1')
+//const liczba2 = document.querySelector('#liczba2')
 const przeliczBtn = document.querySelector('#przelicz')
 const WynikiPojemnik = document.querySelector('#wyniki')
 
+console.dir(liczba1)
 przeliczBtn.addEventListener('click', () => {
-    console.log(liczba1.value)
-    WynikiPojemnik.innerHTML = liczba1.value
+    console.log(+liczba1.value)
+    //parseInt(liczba1.valule)
+    WynikiPojemnik.innerHTML = 'Wynik: ${liczba1.value}'
 })
 //Math.min(), .max()
