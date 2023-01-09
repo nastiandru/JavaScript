265f62cbb9eb3929389cf3c16f87396a698bab0d
unknown <nastia.andr070703@gmail.com>
2023-01-09 11:18:03 +0200

    Update main.js

--- project2/main.js
+++ project2/main.js
@@ -1,19 +1,18 @@
 const buttons = document.querySelectorAll("[data-slider-button]")
 
 buttons.forEach(button => {
-    button.addEventListner("click", () => {
-        const offset = button.dataset.sliderButton === "next" ? 1 : -1
-        const slides = button
-          .closest("[data-slider]")
-          .querySelector("[data-slides]")
-        
-        const activeSlide = slides.querySelector("[data-active")
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset 
-        if (newIndex < 0) newIndex = slides.children.length - 1
-        if (newIndex >= slides.children.length) newIndex = 0
+  button.addEventListener("click", () => {
+    const offset = button.dataset.sliderButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-slider]")
+      .querySelector("[data-slides]")
 
-        slides.children[newIndex].dataset.active = true
-        delete activeSlide.dataset.active
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
 
-    })
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
 })
