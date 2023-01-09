9340487666a7a01b7be140628adb4da5e31a3bdc
unknown <nastia.andr070703@gmail.com>
2023-01-09 18:52:59 +0200

    Update main.js

--- project2/main.js
+++ project2/main.js
@@ -1,18 +1,95 @@
-const buttons = document.querySelectorAll("[data-slider-button]")
+{
+  const sliders = document.querySelectorAll(".slider");
+  const prevBtn = document.querySelector(".slider-button prev");
+  const nextBtn = document.querySelector(".slider-button next");
 
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    const offset = button.dataset.sliderButton === "next" ? 1 : -1
-    const slides = button
-      .closest("[data-slider]")
-      .querySelector("[data-slides]")
+  const interval = 2800;
+  const animDuration = 600;
 
-    const activeSlide = slides.querySelector("[data-active]")
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset
-    if (newIndex < 0) newIndex = slides.children.length - 1
-    if (newIndex >= slides.children.length) newIndex = 0
+  for (let i = 0; i < sliders.length; ++i) {
+    const slider = sliders[i];
+    const dots = slider.querySelector(".dots");
+    const sliderImgs = slider.querySelectorAll(".img");
 
-    slides.children[newIndex].dataset.active = true
-    delete activeSlide.dataset.active
+    let currImg = 0;
+    let prevImg = sliderImgs.length - 1;
+    let intrvl;
+    let timeout;
+
+    // Creates dots and add listeners to them
+    for (let i = 0; i < sliderImgs.length; ++i) {
+      const dot = document.createElement("div");
+      dot.classList.add("dot");
+      dots.appendChild(dot);
+      dot.addEventListener("click", dotClick.bind(null, i), false);
+    }
+
+    const allDots = dots.querySelectorAll(".dot");
+    allDots[0].classList.add("active-dot");
+
+    sliderImgs[0].style.left = "0";
+    timeout = setTimeout(() => {
+      animateSlider();
+      sliderImgs[0].style.left = "";
+      intrvl = setInterval(animateSlider, interval);
+    }, interval - animDuration);   
+
+    /**
+     * Animates images
+     * @param {number} [nextImg] - index of next image to show
+     * @param {boolean} [right = false] - animate to right
+     */
+    function animateSlider(nextImg, right) {
+      if (!nextImg)
+        nextImg = currImg + 1 < sliderImgs.length ? currImg + 2 : 1;
+
+      --nextImg;
+      sliderImgs[prevImg].style.animationName = "";
+
+      if (!right) {
+        sliderImgs[nextImg].style.animationName = "leftNext";
+        sliderImgs[currImg].style.animationName = "leftCurr";
+      } 
+      else {
+        sliderImgs[nextImg].style.animationName = "rightNext";
+        sliderImgs[currImg].style.animationName = "rightCurr";
+      }
+
+      prevImg = currImg;
+      currImg = nextImg;
+
+      currDot = allDots[currImg];
+      currDot.classList.add("active-dot");
+      prevDot = allDots[prevImg];
+      prevDot.classList.remove("active-dot");
+    }
+
+    /**
+     * Decides if animate to left or right and highlights clicked dot
+     * @param {number} num - index of clicked dot
+     */
+    function dotClick(num) {
+      if (num == currImg)
+        return false;
+
+      clearTimeout(timeout);
+      clearInterval(intrvl);
+
+      if (num > currImg)
+        animateSlider(num + 1);
+      else
+        animateSlider(num + 1, true);
+
+      intrvl = setInterval(animateSlider, interval);
+    }
+  }
+
+  nextBtn.addEventListener('click', ()=>{
+
   })
-})
+
+  prevBtn.addEventListener('click', () => {
+
+  })
+
+}
