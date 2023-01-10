f66f0fe33f3aef55db50db30b621f74c9da72941
unknown <nastia.andr070703@gmail.com>
2023-01-10 17:41:17 +0200

    Update note.js

--- project4/note.js
+++ project4/note.js
@@ -5,3 +5,32 @@ titleEl = document.querySelector('input')
 descEl = document.querySelector('textarea')
 addBtn = document.querySelector('button ')
 
+const notes = JSON.parse(localStorage.getItem('notes') || '[]');
+let isUpdate = false, updateId;
+
+function showNotes() {
+    document.querySelectorAll('.note').forEach(note => note.remove());
+    notes.forEach((note, index)=>{
+        let liEl=`<li class="note">
+                        <div class="details">
+                            <p>${note.title}</p>
+                            <span>${note.description}</span>
+                        </div>
+                        <div class="bottom-content">
+                            <span>${note.date}</span>
+                            <div class="settings">
+                                <i onClick="updateNote(${index}, '${note.title}', '${note.description}')"  class="uil uil-edit"></i>
+                                <i onClick="deleteNote(${index})" class="uil uil-trash"></i>
+                            </div>
+                        </div>
+                    </li>`;
+        addBox.insertAdjacentHTML('afterend', liEl);
+    });
+}
+
+showNotes();
+
+addBox.addEventListener('click', ()=>{
+    titleEl.focus();
+    container.classList.add('show')
+});
